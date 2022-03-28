e629cb5abd716f7ea225b797ac681d52e52f2ad7
olima284 <olivialima.student@careerdevs.com>
2022-03-28 17:32:45 -0400

    intial commit

--- src/com/careerdevs/HankerRank/Day01HelloWorld.java
+++ src/com/careerdevs/HankerRank/Day01HelloWorld.java
@@ -2,7 +2,8 @@ package com.careerdevs.HankerRank;
 
 import java.util.Scanner;
 
-public class Day01HelloWorld {public static void main(String[] args) {
+public class Day01HelloWorld {
+    public static void main(String[] args) {
 
     // Create a Scanner object to read input from stdin.
     Scanner scan = new Scanner(System.in);
@@ -12,7 +13,7 @@ public class Day01HelloWorld {public static void main(String[] args) {
 
     // Close the scanner object, because we've finished reading
     // all of the input from stdin needed for this challenge.
-		scan.close();
+        scan.close();
 
     // Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
@@ -20,5 +21,18 @@ public class Day01HelloWorld {public static void main(String[] args) {
     // TODO: Write a line of code here that prints the contents of inputString to stdout.
         System.out.println(inputString);
   }
-}
+
+    public static void Day7Arrays() {
+                Scanner in = new Scanner(System.in);
+                int n = in.nextInt();
+                int[] arr = new int[n];
+                for(int i=0; i < n; i++){
+                    arr[i] = in.nextInt();
+                }
+                in.close();
+                for(int i = n - 1; i > -1; i--){
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
 
