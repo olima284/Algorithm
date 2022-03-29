572072e190df3299152e383c72cad06d02fc70d6
olima284 <olivialima.student@careerdevs.com>
2022-03-29 17:50:34 -0400

    day5

---
+++ src/com/careerdevs/HankerRank/Day5Loops.java
@@ -0,0 +1,17 @@
+package com.careerdevs.HankerRank;
+
+import java.util.Scanner;
+
+public class Day5Loops {
+    public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+                int n = scanner.nextInt();
+                for(int i=1;i<=10;i++){
+                    System.out.println( n +" x "+ i +" = "+n*i);
+                }
+                scanner.close();
+            }
+
+        }
+
--- src/com/careerdevs/HankerRank/Day7Arrays.java
+++ src/com/careerdevs/HankerRank/Day7Arrays.java
@@ -10,7 +10,7 @@ public class Day7Arrays {
         for(int i=0; i < n; i++){
             arr[i] = in.nextInt();
         }
-        in.close();
+
         for(int i = n - 1; i > -1; i--){
             System.out.print(arr[i] + " ");
         }
