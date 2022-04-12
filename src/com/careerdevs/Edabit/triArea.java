6b7b30a8b091d12bb87b795bf11abf5b4527f913
olima284 <olivialima.student@careerdevs.com>
2022-04-12 19:16:28 -0400

    triArealtiples

---
+++ src/com/careerdevs/Edabit/triArea.java
@@ -0,0 +1,19 @@
+package com.careerdevs.Edabit;
+
+public class triArea {
+    public static void main(String[] args) {
+        class Challenge {
+            public static int triArea(int base, int height) {
+                return (base*height)/2;
+            }
+        }
+    }
+}
+
+//Define base and height
+//base= int base
+//height= int height
+
+//return area formula.
+ //area = (b*h)/2
+
